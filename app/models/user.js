0d0985fb1443d85a11a23d93e2f0eb4f450019c4
Kyle Fredin <kfredin@efolder.net>
2019-05-27 10:44:56 -0600

    update user model and factory

--- app/models/user.js
+++ app/models/user.js
@@ -4,6 +4,8 @@ export default DS.Model.extend({
   firstName: DS.attr("string"),
   lastName: DS.attr("string"),
 
+  emailAddress: DS.attr("string"),
+
   organization: DS.belongsTo("organization"),
   documents: DS.hasMany("document")
 });
