a83a6eb447f2ec110214e6d42d55d10dd75a37ae
Kyle Fredin <kfredin@efolder.net>
2019-05-28 14:57:11 -0600

    add users.edit controller

---
+++ app/controllers/users/edit.js
@@ -0,0 +1,13 @@
+import Controller from '@ember/controller';
+
+export default Controller.extend({
+  actions: {
+    cancel() {
+      // reset changes
+    },
+
+    saveUser() {
+      // grab the model and save the user!
+    }
+  }
+});
